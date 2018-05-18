0bd5b2afca32459628aa2d6b327b363cbdc7553a
zhenbeningg <18720979478@163.com>
2018-05-19 00:25:20 +0800

    first commit

---
+++ test/src/main/java/com/aben/test/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.aben.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
